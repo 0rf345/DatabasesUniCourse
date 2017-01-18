c436d89a01aacb0e574583c8f24c00e85a9fca5d
Orfeas Kalipolitis <csd3285@csd.uoc.gr>
2017-01-18 18:12:48 +0200

    basic comm with servlet

---
+++ CS360Project/web/javascript.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function loginPOST() {
+    var usern = $("#usern").val();
+    var userp = $("#userp").val();
+    var xhr = new XMLHttpRequest();
+    alert("a");
+    xhr.open('POST', 'login?usern=' + usern + '&userp=' + userp);
+    xhr.onload = function() {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            // Everything OK
+            if(xhr.responseText === "authenticated") {
+                
+            }else{
+                $(body).html("GET OUTTA TOWN");
+            }
+        }
+    };
+}
