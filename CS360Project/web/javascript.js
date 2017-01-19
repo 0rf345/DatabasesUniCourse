eb6063a77f913a5cff414bc41a631a976f620d8e
Orfeas Kalipolitis <csd3285@csd.uoc.gr>
2017-01-19 19:45:35 +0200

    Pretty login and register pagees. Register not completed

--- CS360Project/web/javascript.js
+++ CS360Project/web/javascript.js
@@ -17,6 +17,28 @@ function loginPOST() {
                 $("body").html("LOGGED IN");
             }else{
                 alert("unauthorized");
+                $("body").html("GET OUTTA TOWN");
+            }
+        }
+    };
+    
+    xhr.setRequestHeader('ContentType','application/x-www-form-urlencoded');
+    xhr.send();
+}
+
+function registerPOST() {
+    var usern = $("#usern").val();
+    var userp = $("#userp").val();
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'register?usern=' + usern + '&userp=' + userp);
+    xhr.onload = function() {
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            // Everything OK
+            if(xhr.responseText === "authenticated") {
+                alert("authorized");
+                $("body").html("LOGGED IN");
+            }else{
+                alert("unauthorized");
             }
         }
     };
@@ -26,6 +48,12 @@ function loginPOST() {
 }
 
 
+
 function registerPage() {
     window.location.href = "register.html";
+    $("#companyStuff").hide();
+}
+
+function loginPage() {
+    window.location.href = "index.html";
 }
