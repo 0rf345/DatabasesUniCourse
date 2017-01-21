ddb5e3f516fa388d82d8e0bc158e97d29b9ba80a
Orfeas Kalipolitis <csd3285@csd.uoc.gr>
2017-01-21 20:07:57 +0200

    check for required fields during registration

--- CS360Project/web/javascript.js
+++ CS360Project/web/javascript.js
@@ -36,22 +36,45 @@ function loginPOST() {
 
 function registerPOST() {
     // Every client
+    if(document.getElementById("usern").checkValidity() === false) {
+        alert("Please input username");
+        return;
+    }else if(document.getElementById("userp").checkValidity() === false) {
+        alert("Please input password");
+        return;
+    }else if($('input[name="accountType"]:checked').length === 0) {
+        alert("Please choose an account type");
+        return;
+    }
+    
+    
     var usern = $("#usern").val();
     var userp = sha1($("#userp").val());
     var client= $('input[name="accountType"]:checked').val();
     
-    var first = $("#first").val();
-    var last  = $("#last").val();
     var object = new Object();
-    
+    var first;
+    var last;
     // Company Specific
     var firsts = {};
     var lasts = {}; 
     if (client === 'company') {
+        if(document.getElementById("howMany").checkValidity() === false) {
+            alert("Please input number of employees between 1 and 5");
+            return;
+        }
         var num = $("#howMany").val();
         object.num = num;
         var arr = {};
         for(var i = 0; i < num; i++) {
+            if(document.getElementById("first"+i).checkValidity() === false) {
+                alert("Please input first name for empoyee #" +(i+1));
+                return;
+            }
+            if(document.getElementById("last"+i).checkValidity() === false) {
+                alert("Please input last name for empoyee #" +(i+1));
+                return;
+            }
             firsts[i] = $("#first"+i).val();
             lasts[i] = $("#last"+i).val();
             var obj = new Object();
@@ -60,15 +83,26 @@ function registerPOST() {
             arr[i] = obj;
         }
         object.employees = arr;
+        if(document.getElementById("name").checkValidity() === false) {
+                alert("Please input name for your company");
+                return;
+            }
         object.name = $("#name").val();
     }else{
         // Client is either individual or merchant
+        if(document.getElementById("first").checkValidity() === false) {
+            alert("Please input a first name");
+            return;
+        }else if(document.getElementById("last").checkValidity() === false) {
+            alert("Please input a last name");
+            return;
+        }
+        first = $("#first").val();
+        last  = $("#last").val();
         object.first = first;
         object.last  = last;
     }
     
-    object.first = first;
-    object.last  = last;
     object.usern = usern;
     object.userp = userp;
     object.client= client;
