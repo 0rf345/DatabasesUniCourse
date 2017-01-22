0b2ede97182e1c9d70792c6b088e0cc3b4dcc0d4
Constantine K <looselyrigorous@gmail.com>
2017-01-22 16:45:15 +0200

    Add method for rudimentary table validation
    Only existence is checked so it can't be called validation per se. Who cares.

--- CS360Project/src/java/edu/ppg/cs360proj/cs360db/DBCommon.java
+++ CS360Project/src/java/edu/ppg/cs360proj/cs360db/DBCommon.java
@@ -7,9 +7,16 @@
  */
 package edu.ppg.cs360proj.cs360db;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -21,8 +28,38 @@ public class DBCommon {
 	private static final int PORT = 3306;
 	private static final String UNAME = "CS360_CCC";
 	private static final String PASSWD = "CS360_CCC";
+
+	public static boolean validateDB() {
+		try {
+			Connection con = DBCommon.getConnection();
+			Statement stmt = con.createStatement();
+			DatabaseMetaData dbmd = con.getMetaData();
+			ResultSet res;
+
+			res = dbmd.getTables(null, null, "individual", null);
+			if(!res.next()) {
+				IndividualDB.genTables();
+			}
+
+			res = dbmd.getTables(null, null, "company", null);
+			if(!res.next()) {
+				CompanyDB.genTables();
+			}
+
+			res = dbmd.getTables(null, null, "merchant", null);
+			if(!res.next()) {
+				MerchantDB.genTables();
+			}
+
+			// TODO: Add clauses for "TransactionDB"
+			return true;
+		} catch (SQLException ex) {
+			Logger.getLogger(DBCommon.class.getName()).log(Level.SEVERE, null, ex);
+			return false;
+		}
+	}
 	
-	public static Connection getConnection() {
+	public static Connection getConnection() throws SQLException {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			return DriverManager.getConnection(URL + ":" + PORT + "/" + DATABASE
